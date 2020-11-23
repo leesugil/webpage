ccd2ed530087f73a36e058053f4a52a4c1a64856
steve84lee <steve84lee@gmail.com>
2020-11-23 18:09:18 -0500

    wording

--- projects/js/draw_surface.js
+++ projects/js/draw_surface.js
@@ -22,8 +22,8 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color( 0xf0f0f0 );
 
 // source of light
-function setLight(scene) {
-	let light = new THREE.DirectionalLight(0xddffdd, 1);
+function setLight(scene, color = 0xddffdd) {
+	let light = new THREE.DirectionalLight(color, 1);
 	let helper = new THREE.DirectionalLightHelper( light, 1 );
 	light.position.set( 5, 6, 2 );
 	let targetObject = new THREE.Object3D();
@@ -39,14 +39,14 @@ let helper = lightInfo['helper'];
 
 
 // prepare a camera
-function setCamera(scene, zoom = 50, x = 9, y = 5, z = 7) {
+function setCamera(scene, zoom = 50, x = 9, y = 5, z = 7, t_x = 0, t_y = 2, t_z = 0) {
 	let camera = new THREE.OrthographicCamera( -window.innerWidth / 2, window.innerWidth / 2, window.innerHeight / 2, -window.innerHeight / 2, 1, 500 );
 	camera.zoom = zoom;
 	camera.updateProjectionMatrix();
 	scene.add( camera );
 	// mathematician's (x, y, z) should be implemented here as (y, z, x).
 	camera.position.set( y, z, x );
-	camera.lookAt( 0, 2, 0 );
+	camera.lookAt( t_x, t_y, t_z );
 	return camera;
 }
 let camera = setCamera(scene);
@@ -180,7 +180,7 @@ let light2 = lightInfo2['light'];
 let helper2 = lightInfo2['helper'];
 
 //let camera2 = setCamera(scene2, 50, 0, 0, 10);
-let camera2 = setCamera(scene2, 80);
+let camera2 = setCamera(scene2, 80, 9, 5, 5, 0, 0, 0);
 
 setCoordAxes(scene2);
 
@@ -279,14 +279,6 @@ function g(x, y) {
 	return Math.sqrt(36 - x**2 - y**2);
 }
 
-function x_coord(u, v) {
-	return u * Math.cos(v);
-}
-
-function y_coord(u, v) {
-	return u * Math.sin(v);
-}
-
 function drawSurface3(f, scene, u_max = Math.sqrt(35), n_intervals_u = 20, v_max = 5, n_intervals_v = 20, edges = true) {
 	// draw the surface using triangulation
 	material = new THREE.MeshLambertMaterial( { color : 0xcccccc, vertexColors: true, side: THREE.DoubleSide } );
@@ -310,7 +302,7 @@ function drawSurface3(f, scene, u_max = Math.sqrt(35), n_intervals_u = 20, v_max
 		let dy = (y_max - y_min) / n_intervals_v;
 		for (j=0; j< n_vertices_v; j++) {
 			let y = y_min + (j * dy);
-			let z = g(x, y);
+			let z = f(x, y);
 			vertices.push(y, z, x);
 			colors.push(1, 1, 1);
 		}
@@ -344,10 +336,190 @@ function drawSurface3(f, scene, u_max = Math.sqrt(35), n_intervals_u = 20, v_max
 	scene.add(mesh);
 }
 
-//drawSurface2(g, scene2);
-drawSurface3(g, scene3, Math.sqrt(35), 30, 5, 30, false);
+drawSurface3(g, scene3, Math.sqrt(35), 40, 5, 40, false);
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+//////////// SCENE 4 //////////////////////////
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+let container4 = document.getElementById('canvas4');
+let renderer4 = getRenderer(container4);
+
+const scene4 = new THREE.Scene();
+scene4.background = new THREE.Color( 0xf0f0f0 );
+
+let lightInfo4 = setLight(scene4, 0xff5500);
+let light4 = lightInfo4['light'];
+let helper4 = lightInfo4['helper'];
+
+let camera4 = setCamera(scene4, 50);
+
+setCoordAxes(scene4);
+
+function drawSurface4(f, scene, u_max = 6, n_intervals_u = 20, v_max = 5, n_intervals_v = 20, edges = true) {
+	// draw the surface using triangulation
+	material = new THREE.MeshLambertMaterial( { color : 0xeeeeee, vertexColors: true, side: THREE.DoubleSide } );
+
+	geometry = new THREE.BufferGeometry();
+	let indices = [];
+	let vertices = [];
+	let normals = [];
+	let colors = [];
+	let du = 2*u_max / n_intervals_u;
+	let dv = 2*v_max / n_intervals_v;
+	let n_vertices_u = n_intervals_u + 1;
+	let n_vertices_v = n_intervals_v + 1;
+	
+	// use the parametrized domain!
+	for (i=0; i<n_vertices_u; i++) {
+		let x = -u_max + (i * du);
+		let y_min = -Math.sqrt(36 - x**2);
+		let y_max = Math.sqrt(36 - x**2);
+		let dy = (y_max - y_min) / n_intervals_v;
+		for (j=0; j< n_vertices_v; j++) {
+			let y = y_min + (j * dy);
+			let z = f(x, y);
+			vertices.push(y, z, x);
+			colors.push(1, 1, 1);
+		}
+	}
+
+	// generate indices
+	for (i = 0; i < n_intervals_u; i++) {
+		for (j = 0; j < n_intervals_v; j++) {
+			let a = (i*n_vertices_v) + j;
+			let b = ((i+1)*n_vertices_v) + j;
+			let c = (i*n_vertices_v) + j+1;
+			let d = ((i+1)*n_vertices_v) + j+1;
+			indices.push(a, b, c);
+			indices.push(c, b, d);
+		}
+	}
+
+	geometry.setIndex(indices);
+	geometry.setAttribute( 'position', new THREE.Float32BufferAttribute(vertices, 3) );
+	geometry.setAttribute( 'color', new THREE.Float32BufferAttribute(colors, 3) );
+
+	geometry.computeVertexNormals();
+	
+	if (edges) {
+		const edges = new THREE.EdgesGeometry( geometry );
+		const line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0xf0f0f0 } ) );
+		scene.add( line );
+	}
+
+	let mesh = new THREE.Mesh( geometry, material );
+	scene.add(mesh);
+}
+
+drawSurface4(g, scene4, 6, 40, 6, 40, false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+//////////// SCENE 5 //////////////////////////
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+let container5 = document.getElementById('canvas5');
+let renderer5 = getRenderer(container5);
+
+const scene5 = new THREE.Scene();
+scene5.background = new THREE.Color( 0xf0f0f0 );
+
+let lightInfo5 = setLight(scene5);
+let light5 = lightInfo5['light'];
+let helper5 = lightInfo5['helper'];
+
+let camera5 = setCamera(scene5, 80, 9, 5, 5, 0, 0, 0);
+
+setCoordAxes(scene5);
+
+function drawSurface5(f, scene, u_max = 5, n_intervals_u = 20, v_max = 5, n_intervals_v = 20, edges = true) {
+	// draw the surface using triangulation
+	material = new THREE.MeshLambertMaterial( { color : 0xcccccc, vertexColors: true, side: THREE.DoubleSide } );
+
+	geometry = new THREE.BufferGeometry();
+	let indices = [];
+	let vertices = [];
+	let normals = [];
+	let colors = [];
+	let du = 2*u_max / n_intervals_u;
+	let dv = 2*v_max / n_intervals_v;
+	let n_vertices_u = n_intervals_u + 1;
+	let n_vertices_v = n_intervals_v + 1;
+	
+	// use the parametrized domain!
+	for (i=0; i<n_vertices_u; i++) {
+		let x = -u_max + (i * du);
+		let y_min = -Math.sqrt(25 - x**2);
+		let y_max = Math.sqrt(25 - x**2);
+		let dy = (y_max - y_min) / n_intervals_v;
+		for (j=0; j< n_vertices_v; j++) {
+			let y = y_min + (j * dy);
+			let z = f(x, y);
+			vertices.push(y, z, x);
+			colors.push(1, 1, 1);
+		}
+	}
+
+	// generate indices
+	for (i = 0; i < n_intervals_u; i++) {
+		for (j = 0; j < n_intervals_v; j++) {
+			let a = (i*n_vertices_v) + j;
+			let b = ((i+1)*n_vertices_v) + j;
+			let c = (i*n_vertices_v) + j+1;
+			let d = ((i+1)*n_vertices_v) + j+1;
+			indices.push(a, b, c);
+			indices.push(c, b, d);
+		}
+	}
+
+	geometry.setIndex(indices);
+	geometry.setAttribute( 'position', new THREE.Float32BufferAttribute(vertices, 3) );
+	geometry.setAttribute( 'color', new THREE.Float32BufferAttribute(colors, 3) );
+
+	geometry.computeVertexNormals();
+	
+	if (edges) {
+		const edges = new THREE.EdgesGeometry( geometry );
+		const line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0xf0f0f0 } ) );
+		scene.add( line );
+	}
+
+	let mesh = new THREE.Mesh( geometry, material );
+	scene.add(mesh);
+}
+
+drawSurface5(f, scene5, 5, 40, 5, 40, false);
 
 
 
@@ -366,17 +538,22 @@ drawSurface3(g, scene3, Math.sqrt(35), 30, 5, 30, false);
 
 
 // i'm actually not animating anything.
-i = 0;
+//i = 0;
 const animate = function () {
 	requestAnimationFrame( animate );
 	
-	helper.update();
-	helper2.update();
+	//helper.update();
+	//helper2.update();
 	renderer.render( scene, camera );
 	renderer2.render( scene2, camera2 );
 	renderer3.render( scene3, camera3 );
-	i += 0.05;
-	i = i % 10;
+	renderer4.render( scene4, camera4 );
+	renderer5.render( scene5, camera5 );
+	//i += 0.05;
+	//i = i % 10;
 };
 
 animate()
+
+
+// next job: make key components (scene, camera, etc) as classes with attributes (position, target, etc.)!
